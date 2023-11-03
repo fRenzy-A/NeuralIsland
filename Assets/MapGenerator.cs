a7ab803dc7b6f74f77573b8cc60fd330d741f7b2
Nathan A <fRenzyA05@gmail.com>
2023-11-03 12:47:37 -0300

    small code adds

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -15,7 +15,7 @@ public class MapGenerator : MonoBehaviour
     
     void Start()
     {
-        
+        GenerateIsland();
     }
 
     // Update is called once per frame
@@ -28,6 +28,7 @@ public class MapGenerator : MonoBehaviour
     {
 
         List<GameObject> edgeBlocks = new List<GameObject>();
+        List<GameObject> blocks = new List<GameObject>();
 
         float spreadDecay = 1;
         //create an origin block for the rest of the blocks to spread in
@@ -35,13 +36,34 @@ public class MapGenerator : MonoBehaviour
         //slap it on a random place // placed on center for testing purposes. TODO: remove this comment after its solved
         originBlock.transform.position = new Vector3(0, 0, 0);
 
-        //put it in a list. spawn other blocks surrounding itself. those blocks will then be added to the list. remove the block itself on the list right after
-        //the blocks that are now in the list will do the same to surround itself with blocks, checking other neighbor blocks in order not to duplicate itself.
+        //put it in a list.
+        blocks.Add(originBlock);
+        edgeBlocks.Add(originBlock);
+
+        //spawn other blocks surrounding itself. those blocks will then be added to the list. remove the block itself on the list right after
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject block = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            block.transform.position = new Vector3();
+        }
+        
+        
+        /*block.transform.position = new Vector3(0, 0, 1);
+        block.transform.position = new Vector3(-1, 0, 0);
+        block.transform.position = new Vector3(0, 0, -1);*/
+
+
+        //the blocks that are now in the list will do the same to surround itself with blocks, checking other neighbor blocks in order not to duplicate to already taken positions.
         //those blocks will then remove itself from the list after 
-        while (spreadDecay > 0)
+        /*while (spreadDecay > 0)
         {
 
-        }
+        }*/
+
+        /*foreach (GameObject edgeBlock in edgeBlocks)
+        {
+            if (edgeBlock != blocks)
+        }*/
 
     }
 
