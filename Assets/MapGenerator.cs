7b26cdab1b62d860b0c90f64246e8db5cef29210
Nathan A <fRenzyA05@gmail.com>
2023-11-02 23:57:16 -0300

    tiny code revision

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Net.NetworkInformation;
 using System.Runtime.Serialization.Formatters;
 using UnityEngine;
 
@@ -7,11 +9,13 @@ public class MapGenerator : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    Vector3[] nodeOrigin;
-    GameObject[] primOrigin = new GameObject[10];
+
+    List<GameObject> islands = new List<GameObject>();
+
+    
     void Start()
     {
-        SetIslandOrigin();
+        
     }
 
     // Update is called once per frame
@@ -20,18 +24,37 @@ public class MapGenerator : MonoBehaviour
         
     }
 
-    void SetIslandOrigin()
+    void GenerateIsland()
     {
-        for (int i = 0; i < primOrigin.Length; i++)
+
+        List<GameObject> edgeBlocks = new List<GameObject>();
+
+        float spreadDecay = 1;
+        //create an origin block for the rest of the blocks to spread in
+        GameObject originBlock = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //slap it on a random place // placed on center for testing purposes. TODO: remove this comment after its solved
+        originBlock.transform.position = new Vector3(0, 0, 0);
+
+        //put it in a list. spawn other blocks surrounding itself. those blocks will then be added to the list. remove the block itself on the list right after
+        //the blocks that are now in the list will do the same to surround itself with blocks, checking other neighbor blocks in order not to duplicate itself.
+        //those blocks will then remove itself from the list after 
+        while (spreadDecay > 0)
         {
-            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            go.transform.position = new Vector3(Random.Range(-50, 50), Random.Range(-50, 50), Random.Range(-50, 50));
-            primOrigin[i] = go;
+
+        }
+
+    }
+
+    struct BlockCoord
+    {
+        public float blockX;
+        public float blockY;
+
+        public BlockCoord(float x, float y)
+        {
+            blockX = x;
+            blockY = y;
         }
     }
 
-    void SpreadOutIslandFloor()
-    {
-        
-    }
 }
